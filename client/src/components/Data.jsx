bd045fd64d84534dd92f7af2d5049e57273de6ce
Daniela FloresGomez <danielaflores@syn-172-100-090-010.res.spectrum.com>
2024-10-17 19:52:54 -0600

    Comma

--- client/src/components/Data.jsx
+++ client/src/components/Data.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Data({ strings }) {
   return <div className="max-w-md p-4 border rounded-md gap-2 grid grid-cols-3">{strings.map((comb) =>
-    <p>{comb}</p>
+    <p>{comb},</p>
   )}</div>;
 }
 
